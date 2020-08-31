b9751d58d59ee2883d7c2edbc2202cb50d40b3f8
abduragim <a.shtanchaev@neurodatalab.com>
2020-08-31 13:32:32 +0300

    commit leasson 14

--- hello.py
+++ hello.py
@@ -1,10 +1,32 @@
 import numpy as np
 from joblib import dump, load
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, abort, redirect, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 knn = load('irismodel.joblib') 
+
+
+
+def predict(data):
+    """ Predicts type of iris flower
+    Arguments:
+        data: a list of numbers seperated with commas
+    Ruturns
+        int: predicted class
+    """
+    data = data.split(',')
+    data = [float(s) for s in data]
+    result = str(knn.predict(np.array(data).reshape(1,-1)).item(0))
+    return result
+
+@app.route('/badrequest400')
+def bad_request():
+    abort(400)
+
+@app.route('/ind')
+def h_world():
+    return render_template('index.html')
 
 @app.route('/')
 def hello_world():
@@ -35,3 +57,15 @@ def iris(data):
 def show_im():
     # return "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg/440px-Kosaciec_szczecinkowaty_Iris_setosa.jpg', alt='Flowers in Chania'>"
     return '<img src="/static/setosa.jpg" alt="setosa">'
+
+
+@app.route('/iris_post', methods=['POST'])
+def iris_post():
+    try:
+        content = request.get_json()
+        content = content['flower']
+        rlt = {"class" : predict(content)}
+    except:
+        return redirect(url_for('bad_request'))
+    return jsonify(rlt)
+
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 docker exec -it flask-hello_flask_1 bash
+
+docker exec -it flask-hello_flask_1 python train_model.py
+
+curl --header "Content-Type: application/json" \
+--request POST \
+--data '{"flower":"2,4,5,7"}' \
+http://localhost:5000/iris_post
