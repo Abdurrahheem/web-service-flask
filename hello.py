fe1d5d43abd7dc5f08f90db02d4d40dac1847591
abduragim <a.shtanchaev@neurodatalab.com>
2020-08-31 19:48:40 +0300

    lesson 19

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import pandas as pd
 from joblib import dump, load
-from flask import Flask, request, jsonify, render_template, abort, redirect, url_for, send_file
+from flask import Flask, request, jsonify, render_template, abort, redirect, url_for, send_file, flash
 from markupsafe import escape
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -68,7 +68,6 @@ def show_im():
     # return "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg/440px-Kosaciec_szczecinkowaty_Iris_setosa.jpg', alt='Flowers in Chania'>"
     return '<img src="/static/setosa.jpg" alt="setosa">'
 
-
 @app.route('/iris_post', methods=['POST'])
 def iris_post():
     try:
@@ -101,3 +100,38 @@ def submit():
                         as_attachment=True)
         # return('<h1> File Uploaded </h1>')
     return render_template('submit.html', form=form)
+
+UPLOAD_FOLDER = ''
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=('GET', 'POST'))
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename + 'fileuploaded')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return '<h1> file uploaded </h1>'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
